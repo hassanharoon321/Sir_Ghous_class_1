b3366a5ca27f3cfa19502450596551096c260f0f
hassan haroon <hassanharoon321@gmail.com>
2020-12-01 13:28:12 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Header extends React.Component{
+  render(){
+    return(
+      <div className="header">
+      <h2 >Header</h2>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component{
+  render(){
+    // const name = "Hassan";
+    // const todos = [{name:"Hassan"},{name:"basit"}]
+    return(
+      <div>
+      
+        {/* <ul>
+        {todos.map((item,index)=>{
+          return <li key={index}>{item.name}</li>
+          
+      })}
+      </ul> */}
+
+        <Header/>
+        <h1>Main Component</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
